acd41d77ddfb9c6b3173257480d4e43db5bc3df3
shuan0 <jmantunia01@gmail.com>
2024-08-24 16:21:05 -0300

    fix: Root url redirects to home

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda _: redirect('home')),
     path('account/', include('account.urls')),
     path('chat/', include('chat.urls'))
 ]
